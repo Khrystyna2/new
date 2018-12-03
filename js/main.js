a3442f761c184b5c9166a21a738518d407ad9bc4
Khrystyna Khozhai <l.xrustja@gmail.com>
2018-12-03 17:52:32 +0200

    added new image and style for page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
-
 const second = 1000,
 minute = second * 60,
 hour = minute * 60,
 day = hour * 24;
 
-let countDown = new Date('Nov 30, 2019 00:00:00').getTime(),
+let countDown = new Date('Dec 3, 2018 00:00:00').getTime(),
     x = setInterval(function() {
 
-    let now = new Date().getTime(),
-        distance = countDown - now;
+    let now = new Date().getTime();
+        let distance = countDown - now;
 
         document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
+        if (distance <= now ) {
+                document.getElementById('days').innerText = 'hello';
+        }
+    }, second);
 
-// do something later when date is reached
-// if (distance < 0) {
-//  clearInterval(x);
-//  'IT'S MY BIRTHDAY!;
-// }
-    }, second)
+    
+
+        //map
+        function initMap() {
+                var location = {lat: 48.9252, lng: 24.7147};
+                var map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 15,
+                    center: location
+                });
+    
+        }
+
+$(document).ready(function() {
+        $('.competition-category__group-button>button').click(function() {
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+        });
+
+});
