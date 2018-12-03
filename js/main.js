0c5ae62acb630522fb892983b6a02d9f66c2e33b
Khrystyna Khozhai <l.xrustja@gmail.com>
2018-12-03 17:59:18 +0200

    change js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ minute = second * 60,
 hour = minute * 60,
 day = hour * 24;
 
-let countDown = new Date('Dec 3, 2018 00:00:00').getTime(),
-    x = setInterval(function() {
+let countDownStart = new Date('Dec 3, 2018 00:00:00').getTime();
+let countDownEnd = new Date('Dec 05 2018 00:00:00').getTime();
+let x = setInterval(function() {
 
     let now = new Date().getTime();
-        let distance = countDown - now;
+        let distance = countDownEnd - countDownStart;
 
         document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
-        if (distance <= now ) {
-                document.getElementById('days').innerText = 'hello';
+        if (distance <= countDownEnd ) {
+                // document.getElementById('days').innerText = 'hello';
         }
     }, second);
 
